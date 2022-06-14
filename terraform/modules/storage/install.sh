f399389f119580e819f837eecf2d89f1f467c1de
Jeremy Cook <jeremy.cook@cloudacademy.com>
2022-06-15 10:57:48 +1200

    updates

--- terraform/modules/storage/install.sh
+++ terraform/modules/storage/install.sh
@@ -13,18 +13,20 @@ systemctl status postgresql.service
 sudo -i -u postgres psql -c "ALTER USER postgres PASSWORD 'cloudacademy';"
 sudo -i -u postgres psql -c "CREATE DATABASE cloudacademy;"
 
-sudo -i -u postgres psql -c "CREATE TABLE IF NOT EXISTS users(user_id VARCHAR (36) PRIMARY KEY, username VARCHAR (50) UNIQUE NOT NULL, password VARCHAR (50) NOT NULL, created_on TIMESTAMP NOT NULL, last_login TIMESTAMP);"
-sudo -i -u postgres psql -c "CREATE TABLE IF NOT EXISTS comments(id VARCHAR (36) PRIMARY KEY, username VARCHAR (36), body VARCHAR (500), created_on TIMESTAMP NOT NULL);"
+cat > dbsetup.sql << EOF
+CREATE TABLE IF NOT EXISTS users(user_id VARCHAR (36) PRIMARY KEY, username VARCHAR (50) UNIQUE NOT NULL, password VARCHAR (50) NOT NULL, created_on TIMESTAMP NOT NULL, last_login TIMESTAMP);
+CREATE TABLE IF NOT EXISTS comments(id VARCHAR (36) PRIMARY KEY, username VARCHAR (36), body VARCHAR (500), created_on TIMESTAMP NOT NULL);
 
-#insert USERS seed data 
-sudo -i -u postgres psql -c "INSERT INTO users (user_id, username, password, created_on) VALUES ('$(uuidgen)', 'admin', '$(echo secretpassword | md5sum | cut -b-32)', current_timestamp);"
-sudo -i -u postgres psql -c "INSERT INTO users (user_id, username, password, created_on) VALUES ('$(uuidgen)', 'alice', '$(echo password | md5sum | cut -b-32)', current_timestamp);"
-sudo -i -u postgres psql -c "INSERT INTO users (user_id, username, password, created_on) VALUES ('$(uuidgen)', 'bob', '$(echo password | md5sum | cut -b-32)', current_timestamp);"
-sudo -i -u postgres psql -c "INSERT INTO users (user_id, username, password, created_on) VALUES ('$(uuidgen)', 'joe', '$(echo password | md5sum | cut -b-32)', current_timestamp);"
+INSERT INTO users (user_id, username, password, created_on) VALUES ('$(uuidgen)', 'admin', '$(echo -n v3rysecretpassword | md5sum | cut -b-32)', current_timestamp);
+INSERT INTO users (user_id, username, password, created_on) VALUES ('$(uuidgen)', 'alice', '$(echo -n password1 | md5sum | cut -b-32)', current_timestamp);
+INSERT INTO users (user_id, username, password, created_on) VALUES ('$(uuidgen)', 'bob', '$(echo -n password2 | md5sum | cut -b-32)', current_timestamp);
+INSERT INTO users (user_id, username, password, created_on) VALUES ('$(uuidgen)', 'joe', '$(echo -n password3 | md5sum | cut -b-32)', current_timestamp);
 
-#insert COMMENTS seed data 
-sudo -i -u postgres psql -c "INSERT INTO comments (id, username, body, created_on) VALUES ('$(uuidgen)', 'bob', 'good times ahead', current_timestamp);"
-sudo -i -u postgres psql -c "INSERT INTO comments (id, username, body, created_on) VALUES ('$(uuidgen)', 'alice', 'security review required', current_timestamp);"
-sudo -i -u postgres psql -c "INSERT INTO comments (id, username, body, created_on) VALUES ('$(uuidgen)', 'joe', 'lets roll', current_timestamp);"
+INSERT INTO comments (id, username, body, created_on) VALUES ('$(uuidgen)', 'bob', 'good times ahead', current_timestamp);
+INSERT INTO comments (id, username, body, created_on) VALUES ('$(uuidgen)', 'alice', 'security review required', current_timestamp);
+INSERT INTO comments (id, username, body, created_on) VALUES ('$(uuidgen)', 'joe', 'lets roll', current_timestamp);
+EOF
 
-echo fin v1.00!
+sudo -i -u postgres psql -d cloudacademy -f dbsetup.sql
+
+echo fin v1.01!
