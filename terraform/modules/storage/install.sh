d2743c3e1cce7bad4802443b39b885c3f955fa3c
Jeremy Cook <jeremy.cook@cloudacademy.com>
2022-06-16 22:46:12 +1200

    updates

--- terraform/modules/storage/install.sh
+++ terraform/modules/storage/install.sh
@@ -2,10 +2,10 @@
 apt-get -y update
 apt-get -y install jq tree
 
-apt-get -y install postgresql-14
+apt-get -y install postgresql
 #update config to all connections from app server
-echo "listen_addresses='*'" >> /etc/postgresql/14/main/postgresql.conf
-sed -i "s/127\.0\.0\.1\/32/all/g" /etc/postgresql/14/main/pg_hba.conf
+echo "listen_addresses='*'" >> /etc/postgresql/12/main/postgresql.conf
+sed -i "s/127\.0\.0\.1\/32/all/g" /etc/postgresql/12/main/pg_hba.conf
 
 systemctl restart postgresql.service
 systemctl status postgresql.service
